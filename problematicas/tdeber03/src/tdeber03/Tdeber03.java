8cfa8bb851da8313021a3b496fcb4a740f6c79da
jose gabriel <jgabriel2608@gmail.com>
2023-11-23 17:50:41 -0500

    cambios

---
+++ problematicas/tdeber03/src/tdeber03/Tdeber03.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tdeber03;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Tdeber03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+         int limite = 15;
+        int numerador = 1;
+        int denominador = 1;
+        String signo;
+        String cadena = "";
+        double resultado = 0;
+        double v2;
+        do {
+            // para manejar el signo asociado 
+            if (denominador % 2 == 0) {
+                signo = "-";
+                v2 = -numerador/denominador;
+                resultado = resultado + v2;
+            } else {
+                signo = "+";
+                resultado = resultado + numerador/denominador;
+            }
+            
+            cadena = String.format("%s%s%d/%d\n ", 
+                    cadena,
+                    signo,
+                    numerador, 
+                    denominador);
+            denominador = denominador + 2;
+           
+           
+
+        } while (denominador <= limite);
+        
+
+        System.out.println(cadena);
+        System.out.println("resultado"+resultado);
+
+    }
+}
+    
+    
